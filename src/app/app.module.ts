32639d9a2ef8eb1b560adaa056bdb1eddb7164d9
coyote77 <w.andradec@yahoo.com>
2020-10-07 18:44:32 -0500

    compiled first time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
 
@@ -8,18 +8,12 @@ import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 
 @NgModule({
+	declarations: [AppComponent, TopBarComponent, ProductListComponent],
 	imports: [
 		BrowserModule,
 		ReactiveFormsModule,
 		RouterModule.forRoot([{ path: "", component: ProductListComponent }]),
 	],
-	declarations: [AppComponent, TopBarComponent, ProductListComponent],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
